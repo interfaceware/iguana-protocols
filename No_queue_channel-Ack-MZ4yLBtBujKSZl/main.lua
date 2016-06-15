464ac4ab5c4b2fb261b45b82f7529908a185c840
eliot.muir <eliot.muir@interfaceware.com>
2016-06-15 15:50:44 -0400

    Names are changing, so deleting these.

--- No_queue_channel-Ack-MZ4yLBtBujKSZl/main.lua
+++
@@ -1,7 +0,0 @@
--- The main function is the first function called from Iguana.
--- The Data argument will contain the message to be processed.
-function main(Data)
-   -- TODO process message as normal
-   local Ack = ack.generate(Data)
-   ack.send(Ack)
-end
--- No_queue_channel-Filter-3swpVMigJ3NSWt/main.lua
+++
@@ -1,5 +0,0 @@
--- The main function is the first function called from Iguana.
--- The Data argument will contain the message to be processed.
-function main(Data)
-   -- Do nothing - do not enqueue the message.
-end
